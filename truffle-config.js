57048e920891f6c28846cd13bacba0c324f7abdf
Sean Casey <sean@seanjcasey.com>
2019-10-05 17:58:58 +0900

    update truffle config with instructions

--- truffle-config.js
+++ truffle-config.js
@@ -1,33 +1,26 @@
 /**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * truffleframework.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like truffle-hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura API
- * keys are available for free at: infura.io/register
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
+ * Truffle config for Vulcan Swap
  */
-require('dotenv').config()
 
-const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = process.env.INFURA_KEY;
-const fs = require('fs');
-const mnemonic = fs.readFileSync('.secret').toString().trim();
+/**
+ * Uncomment the following lines if you want to deploy to a live network.
+ * You will also need to create a .env file with an INFURA_KEY constant.
+ * You will also need to create a `.secret` file with a private key.
+ */
+// require('dotenv').config()
+// const HDWalletProvider = require('truffle-hdwallet-provider');
+// const infuraKey = process.env.INFURA_KEY;
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync('.secret').toString().trim();
+
 const path = require('path');
 
 module.exports = {
 
-  // Optional dev plugins (install before using)
+  /**
+   * Optional dev plugins (install before using):
+   */
+
   // plugins: [
   //   "truffle-security",
   //   "truffle-plugin-verify"
@@ -38,6 +31,10 @@ module.exports = {
   //   etherscan: process.env.ETHERSCAN_API_KEY
   // },
 
+  /**
+   * Contract compile and build directory options:
+   */
+
   // Need to do this to get contracts inside the create-react-app folder
   contracts_build_directory: path.join(__dirname, 'client/src/contracts'),
 
@@ -45,74 +42,37 @@ module.exports = {
   contracts_directory: path.join(__dirname, 'contracts/deploy'),
 
   /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
+   * Optional dev plugins (install before using):
    */
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    development: {
-      // gasPrice: 1000000000,  // 1 gwei (in wei)
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
-    },
-
-    // develop: {
-    //   accounts: 5,
-    //   defaultEtherBalance: 500,
-    //   network_id: 20
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 7545,
+    //   network_id: "*",
     // },
 
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
-      network_id: 4,       // Rinkeby's id
-      gas: 6500000,        // Rinkeby has a lower block limit than mainnet
-      gasPrice: 2000000000,  // 2 gwei (in wei) (default: 100 gwei)
-      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    }
-
-    // Another network with more advanced options...
-    // advanced: {
-      // port: 8777,             // Custom port
-      // network_id: 1342,       // Custom network
-      // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-      // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-      // from: <address>,        // Account to send txs from (default: accounts[0])
-      // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
+    // rinkeby: {
+    //   provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+    //   network_id: 4,         // Rinkeby's id
+    //   gas: 6500000,          // Rinkeby has a lower block limit than mainnet
+    //   gasPrice: 2000000000,  // 2 gwei (in wei) (default: 100 gwei)
+    //   skipDryRun: true       // Skip dry run before migrations? (default: false for public nets )
+    // }
   },
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
+  /**
+   * Compiler config.
+   */
 
-  // Configure your compilers
   compilers: {
     solc: {
-      version: "0.5.8",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
-        optimizer: {
+      version: "0.5.8", // This is the solidity version specified for all contracts.
+      settings: {
+        optimizer: {  // Needs optimizer because Compound contracts are massive.
           enabled: true,
           runs: 200
         },
-      //  evmVersion: "byzantium"
       }
     }
   }
